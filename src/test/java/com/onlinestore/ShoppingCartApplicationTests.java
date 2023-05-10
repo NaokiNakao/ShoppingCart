93eea3e9e2d4ab2d3216422719003ce3aa8738ac
Naoki Nakao <naokinakao.nn@gmail.com>
2023-05-10 11:05:54 -0400

    Initial Commit

---
+++ src/test/java/com/onlinestore/ShoppingCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlinestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
