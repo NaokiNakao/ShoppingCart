ffaeb490489bfcbfbabaf317e888105776db71fa
Naoki Nakao <naokinakao.nn@gmail.com>
2023-05-13 23:04:26 -0400

    Add email attribute

--- src/main/java/com/onlinestore/user/UserDTO.java
+++ src/main/java/com/onlinestore/user/UserDTO.java
@@ -7,15 +7,17 @@ public class UserDTO {
     private Long id;
     private String firstName;
     private String lastName;
+    private String email;
     private String password;
 
     public UserDTO() {
     }
 
-    public UserDTO(Long id, String firstName, String lastName, String password) {
+    public UserDTO(Long id, String firstName, String lastName, String email, String password) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
+        this.email = email;
         this.password = password;
     }
 
@@ -41,6 +43,14 @@ public class UserDTO {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getPassword() {
