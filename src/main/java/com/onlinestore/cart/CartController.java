8d857b67aa0fd4c8328b750517791e92bab0cabb
Naoki Nakao <naokinakao.nn@gmail.com>
2023-05-16 16:11:48 -0400

    Changes

--- src/main/java/com/onlinestore/cart/CartController.java
+++ src/main/java/com/onlinestore/cart/CartController.java
@@ -1,14 +1,20 @@
 package com.onlinestore.cart;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RestController
 public class CartController {
 
     @GetMapping("/cart")
     public String showCart() {
         return "cart";
+    }
+
+    @PostMapping("catalog{id}")
+    public String addToCart(@RequestBody Cart cart, @PathVariable("id") Long id, @RequestParam("quantity") Integer quantity) {
+        return "redirect:/catalog?success";
     }
 
 }
--- src/main/java/com/onlinestore/cart/CartDAO.java
+++ src/main/java/com/onlinestore/cart/CartDAO.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class CartDAO {
 
-    @Autowired
     private JdbcTemplate jdbcTemplate;
 
     @PostConstruct
